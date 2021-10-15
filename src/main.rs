60391afe9b3d427d8f795c362aa6010354980c95
aQaTL <mmsoltys@outlook.com>
2021-10-15 14:56:07 +0200

    Read images from a filename list

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 #[cfg(feature = "genpdf_")]
 mod genpdf;
-#[cfg(feature = "pdfcreate_")]
-mod pdfcreate;
 #[cfg(feature = "printpdf_")]
 mod printpdf;
 
 fn main() -> anyhow::Result<()> {
-	#[cfg(feature = "pdfcreate_")]
-	crate::pdfcreate::run()?;
 	#[cfg(feature = "printpdf_")]
 	crate::printpdf::run()?;
 	#[cfg(feature = "genpdf_")]
--- src/printpdf.rs
+++ src/printpdf.rs
@@ -1,34 +1,69 @@
+use anyhow::bail;
 use printpdf::{Image, Mm, PdfDocument};
 use std::fs::File;
-use std::io::{Seek, BufWriter};
-
-const A4: (Mm, Mm) = (Mm(210.0), Mm(297.0));
+use std::io::{BufWriter, Read, Seek, Write};
 
 pub fn run() -> anyhow::Result<()> {
-	let (doc, page1, layer1) = PdfDocument::new("printpdf_test", A4.0, A4.1, "Layer 1");
-	let current_layer = doc.get_page(page1).get_layer(layer1);
+	let files = ["assets/benchmark.png", "assets/cbr500r2022.png"];
 
-	let mut image_file = File::open("assets/benchmark.jpg")?;
-	let image = Image::try_from(printpdf::image::codecs::jpeg::JpegDecoder::new(
-		&mut image_file,
-	)?)?;
+	let mut output_file = File::create("printpdf_test.pdf")?;
+	foo(&files, &mut output_file)
+}
 
-	image.add_to_layer(current_layer.clone(), None, None, None, None, None, None);
+fn foo<W: Write>(filenames: &[&str], output: W) -> anyhow::Result<()> {
+	const DPI: f64 = 300.0;
 
-	// let mut image_file = File::open("assets/benchmark.png")?;
-	// let image = Image::try_from(printpdf::image::codecs::png::PngDecoder::new(
-	// 	&mut image_file,
-	// )?)?;
-	image_file.rewind()?;
-	let image = Image::try_from(printpdf::image::codecs::jpeg::JpegDecoder::new(
-		&mut image_file,
-	)?)?;
+	let doc = PdfDocument::empty("img-to-pdf");
 
-	let (page2, layer2) = doc.add_page(A4.0, A4.1, "Layer 2");
-	let current_layer = doc.get_page(page2).get_layer(layer2);
-	image.add_to_layer(current_layer.clone(), None, None, None, None, None, None);
+	for filename in filenames {
+		let image = load_image(filename)?;
 
-	doc.save(&mut BufWriter::new(File::create("printpdf_test.pdf")?))?;
+		let width_mm = px_to_mm(image.image.width.0 as f64, DPI);
+		let height_mm = px_to_mm(image.image.height.0 as f64, DPI);
+
+		let (page_idx, layer_idx) = doc.add_page(Mm(width_mm), Mm(height_mm), "Layer 1");
+		let layer = doc.get_page(page_idx).get_layer(layer_idx);
+		image.add_to_layer(layer.clone(), None, None, None, None, None, Some(DPI));
+	}
+
+	doc.save(&mut BufWriter::new(output))?;
 
 	Ok(())
+}
+
+fn load_image(filename: &str) -> anyhow::Result<Image> {
+	let mut file = File::open(filename)?;
+	let mut file_signature = [0u8; 8];
+	file.read_exact(&mut file_signature)?;
+	match file_signature {
+		[b'B', b'M', _, _, _, _, _, _] => {
+			file.rewind()?;
+			Ok(Image::try_from(
+				printpdf::image::codecs::bmp::BmpDecoder::new(&mut file)?,
+			)?)
+		}
+		[0x89, b'P', b'N', b'G', 0x0D, 0x0A, 0x1A, 0x0A] => {
+			file.rewind()?;
+			Ok(Image::try_from(
+				printpdf::image::codecs::png::PngDecoder::new(&mut file)?,
+			)?)
+		}
+		[0xFF, 0xD8, 0xFF, 0xDB, _, _, _, _]
+		| [0xFF, 0xD8, 0xFF, 0xEE, _, _, _, _]
+		| [0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46] => {
+			file.rewind()?;
+			Ok(Image::try_from(
+				printpdf::image::codecs::jpeg::JpegDecoder::new(&mut file)?,
+			)?)
+		}
+		_ => bail!("{} has an unsupported file format", filename),
+	}
+}
+
+fn px_to_mm(px: f64, dpi: f64) -> f64 {
+	px / dots_per_inch_to_dots_per_mm(dpi)
+}
+
+fn dots_per_inch_to_dots_per_mm(dpi: f64) -> f64 {
+	dpi / 2.54 / 10.0
 }
