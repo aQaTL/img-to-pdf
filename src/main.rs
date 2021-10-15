2ef7fdc2b73efe8a95a8118680a7f826af22c74a
aQaTL <mmsoltys@outlook.com>
2021-10-15 19:35:31 +0200

    Remove unused code

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,8 @@
 mod printpdf;
 mod ui;
 
-#[cfg(feature = "genpdf_")]
-mod genpdf;
-
 fn main() -> anyhow::Result<()> {
 	flexi_logger::Logger::try_with_env_or_str("img_to_pdf=info")?.start()?;
-
-	#[cfg(feature = "genpdf_")]
-	crate::genpdf::run()?;
 
 	crate::ui::run()?;
 
