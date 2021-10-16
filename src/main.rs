be080ae94e0e066fd94d9ae79e99879d0c1eccab
aQaTL <mmsoltys@outlook.com>
2021-10-16 09:01:30 +0200

    Set subsystem to windows only in release builds

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(all(windows, debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(all(windows, not(debug_assertions)), windows_subsystem = "windows")]
 
 mod printpdf;
 mod ui;
